f9f3d44b70bda481d235228e860d20523ff754e5
catedt <catedt@gmail.com>
2020-11-28 06:47:55 +0900

    참조

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,19 @@
 fn main() {
-    let mut x = 5;
-    println!("x의 값: {}", x);
+    let mut s = String::from("hello");
 
-    x = 6;
-    println!("x의 값: {}", x);
+    let len = calculate_length(&s);
+
+    change(&mut s);
+
+    let len2 = calculate_length(&s);
+
+    println!("'{}'의 길이는 {}->{}입니다.", s, len, len2);
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }

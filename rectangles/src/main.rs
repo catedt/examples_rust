7deb9d53b6f29891448b91274d267a32f2d51a98
catedt <catedt@gmail.com>
2021-01-20 08:38:08 +0900

    method

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -8,11 +8,22 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size}
+    }
 }
 fn main() {
-    let rect1 = Rectangle { width : 30, height : 50 };
+    let rect1 = Rectangle::square(3);
+    let rect2 = Rectangle::square(2);
+    let rect3 = Rectangle::square(4);
 
-    println!("rect1: {:#?}", rect1);
+    println!("rect1 in rect2 ? {} ", rect1.can_hold(&rect2));
+    println!("rect1 in rect3 ? {} ", rect1.can_hold(&rect3));
 
     println! (
         "Rectangles Area: {} square pixel",
